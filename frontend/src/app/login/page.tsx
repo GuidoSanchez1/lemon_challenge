82630774776bd62caffffe353d1f9a3096950c97
GuidoSanchez1 <158354168+gsanchezzusaeta@users.noreply.github.com>
2025-07-04 15:45:08 -0300

    .

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -3,7 +3,6 @@
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import useSWRMutation from 'swr/mutation';
 import { loginUser } from '@/lib/api';
